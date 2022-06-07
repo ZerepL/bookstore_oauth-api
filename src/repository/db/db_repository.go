52b6c6f86ed606643fc6caffb066ef9db7cd1942
lucas.perez <pluqui1@gmail.com>
2022-06-07 16:19:32 +0200

    implement GetById

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,8 +1,13 @@
 package db
 
 import (
+	"github.com/ZerepL/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/ZerepL/bookstore_oauth-api/src/domain/access_token"
 	internalErrors "github.com/ZerepL/bookstore_oauth-api/src/utils/errors"
+)
+
+const (
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
 )
 
 type DbRepository interface {
@@ -16,6 +21,16 @@ func NewRepository() DbRepository {
 type dbRepository struct {
 }
 
-func (r *dbRepository) GetById(string) (*access_token.AccessToken, *internalErrors.RestErr) {
-	return nil, internalErrors.NewInternalServerError("Not implemented")
+func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *internalErrors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return nil, internalErrors.NewInternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	var result access_token.AccessToken
+	if err = session.Query(queryGetAccessToken, id).Scan(&result.AccessToken, &result.UserId, &result.ClientId, &result.Expires); err != nil {
+		return nil, internalErrors.NewInternalServerError(err.Error())
+	}
+	return &result, nil
 }
